0dc03e0534876b5c8b93b0306a70c7b6501dd3b0
Rocío (etéreo) <rocio@etereo.io>
2025-06-16 10:39:29 +0200

    update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,8 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`Airflows v2 - Documentation`}
+      description="Airflows v2 - Documentation"
     >
       <HomepageHeader />
       <main></main>
